e71b75fd08f9293c9f8bc9a37231194cb6d17910
alexander <molokan2007@mail.ru>
2024-10-23 22:10:25 +0300

    Add files via upload

---
+++ 9_1.py
@@ -0,0 +1,8 @@
+def apply_all_func(int_list, *functions):
+    result = {}
+    for f in functions:
+        result[f.__name__] = f(int_list)
+    return result
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
